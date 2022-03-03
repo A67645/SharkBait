b2cd8d490403ede327e4d6c59cd4844e3f9d0d01
asergiogomes <ae3654@alunos.uminho.pt>
2022-03-03 22:25:38 +0000

    shark rotation, figure resizing and continuous motion

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ window_width=612
 window_height=437
 
 step=10
-clock_tick_rate=60
+clock_tick_rate=120
 
 # Open a window
 size = (window_width, window_height)
@@ -37,54 +37,88 @@ pygame.display.set_caption("Shark Bait")
 clock = pygame.time.Clock()
 background_image = pygame.image.load("ocean_background.png").convert()
 
-# Load Shark
-img_pre = pygame.image.load('shark_model.png')
-img = pygame.transform.rotozoom(img_pre, 0, 0.2)
-img.convert_alpha()
-shark_width = window_width//2
-shark_height = window_height//2 
+# Load Shark Right
+shark_img_pre_r = pygame.image.load('shark_model_right.png')
+shark_img_r = pygame.transform.rotozoom(shark_img_pre_r, 0, 0.1)
+shark_img_r.convert_alpha()
+shark_img_r_rect = shark_img_r.get_rect()
+shark_x = window_width//2
+shark_y = window_height//2
+
+# Load Shark Left
+shark_img_pre_l = pygame.image.load('shark_model_left.png')
+shark_img_l = pygame.transform.rotozoom(shark_img_pre_l, 0, 0.1)
+shark_img_l.convert_alpha()
+shark_img_l_rect = shark_img_l.get_rect()
+
+# Load Shark Up
+shark_img_pre_u = pygame.image.load('shark_model_up.png')
+shark_img_u = pygame.transform.rotozoom(shark_img_pre_u, 0, 0.1)
+shark_img_u.convert_alpha()
+shark_img_u_rect = shark_img_u.get_rect()
+
+# Load Shark Down
+shark_img_pre_d = pygame.image.load('shark_model_down.png')
+shark_img_d = pygame.transform.rotozoom(shark_img_pre_d, 0, 0.1)
+shark_img_d.convert_alpha()
+shark_img_d_rect = shark_img_d.get_rect()
 
 # Load Fishes
-number = 5
+fish_num = 5
 i = 0
-fish_coords = []
-fish_img_pre = pygame.image.load('fish_model.png')
-fish_img = pygame.transform.rotozoom(fish_img_pre, 0, 0.03)
-fish_img.convert_alpha()
-while i < number:
-    fish_width = randint(0, 600)
-    fish_height = randint(0, 420)
-    fish_coords.append([fish_width, fish_height])
+fish_coords = {}
+fish_imgs = []
+fish_rects = []
+while i < fish_num:
+    fish_x = randint(0, 600)
+    fish_y = randint(0, 420)
+    fish_coords[i] = [fish_x, fish_y]
+    fish_img_pre = pygame.image.load('fish_model.png')
+    fish_img = pygame.transform.rotozoom(fish_img_pre, 0, 0.02)
+    fish_img.convert_alpha()
+    fish_imgs.append(fish_img)
+    fish_rects.append(fish_img.get_rect())
     i+=1
 
 # Game Loop
-pos = [shark_width, shark_height]
+pos = [shark_x, shark_y]
 dead=False
+shark_img = shark_img_r
+shark_rect = shark_img_r_rect
 while(dead==False):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             dead = True
 
-        elif event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                dead = True
+    keys = pygame.key.get_pressed()
 
-            elif event.key == K_RIGHT and pos[0] < 420:
-                pos = [pos[0]+step, pos[1]]
+    pos[0] += keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]
+    pos[1] += keys[pygame.K_DOWN] - keys[pygame.K_UP]
 
-            elif event.key == K_LEFT and pos[0] > 0:
-                pos = [pos[0]-step, pos[1]]
+    if event.type == KEYDOWN:
+        if event.key == K_RIGHT:
+            shark_img = shark_img_r
+            shark_rect = shark_img_r_rect
 
-            elif event.key == K_UP and pos[1] > 0:
-                pos = [pos[0], pos[1]-step]
+        elif event.key == K_LEFT:
+            shark_img = shark_img_l
+            shark_rect = shark_img_l_rect
+        
+        elif event.key == K_UP:
+            shark_img = shark_img_u
+            shark_rect = shark_img_u_rect
 
-            elif event.key == K_DOWN and pos[1] < 350:
-                pos = [pos[0], pos[1]+step]
+        elif event.key == K_DOWN:
+            shark_img = shark_img_d
+            shark_rect = shark_img_d_rect
 
+        i = 0
 
     screen.blit(background_image, [0, 0])
-    screen.blit(img, pos)
-    for coord in fish_coords:
-        screen.blit(fish_img, coord)
+    screen.blit(shark_img, pos)
+    i = 0
+    while i < fish_num:
+        screen.blit(fish_imgs[i], fish_coords[i])
+        i+=1
     pygame.display.flip()
     clock.tick(clock_tick_rate)
