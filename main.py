e6c557fedfacdad0812d16f7f7e18a94522bd76a
A67645 <a67645@alunos.uminho.pt>
2022-03-11 16:17:25 +0000

    diagonal movment

--- main.py
+++ main.py
@@ -63,6 +63,30 @@ shark_img_d = pygame.transform.rotozoom(shark_img_pre_d, 0, 0.1)
 shark_img_d.convert_alpha()
 shark_img_d_rect = shark_img_d.get_rect()
 
+# Load Shark Up Right
+shark_img_pre_ur = pygame.image.load('shark_model_up_right.png')
+shark_img_ur = pygame.transform.rotozoom(shark_img_pre_ur, 0, 0.1)
+shark_img_ur.convert_alpha()
+shark_img_ur_rect = shark_img_ur.get_rect()
+
+# Load Shark Up Left
+shark_img_pre_ul = pygame.image.load('shark_model_up_left.png')
+shark_img_ul = pygame.transform.rotozoom(shark_img_pre_ul, 0, 0.1)
+shark_img_ul.convert_alpha()
+shark_img_ul_rect = shark_img_ul.get_rect()
+
+# Load Shark Down Right
+shark_img_pre_dr = pygame.image.load('shark_model_down_right.png')
+shark_img_dr = pygame.transform.rotozoom(shark_img_pre_dr, 0, 0.1)
+shark_img_dr.convert_alpha()
+shark_img_dr_rect = shark_img_dr.get_rect()
+
+# Load Shark Down Left
+shark_img_pre_dl = pygame.image.load('shark_model_down_left.png')
+shark_img_dl = pygame.transform.rotozoom(shark_img_pre_dl, 0, 0.1)
+shark_img_dl.convert_alpha()
+shark_img_dl_rect = shark_img_dl.get_rect()
+
 # Load Fishes
 fish_num = 5
 i = 0
@@ -95,6 +119,7 @@ while dead != True:
             dead = True
 
         elif event.type == KEYDOWN:
+
             if event.key == K_RIGHT:
                 shark_img = shark_img_r
                 shark_rect = shark_img_r_rect
@@ -110,6 +135,73 @@ while dead != True:
             elif event.key == K_DOWN:
                 shark_img = shark_img_d
                 shark_rect = shark_img_d_rect
+
+            if event.key == K_UP and keys[K_RIGHT] != False:
+                shark_img = shark_img_ur
+                shark_rect = shark_img_ur_rect
+
+            elif event.key == K_UP and keys[K_LEFT] != False:
+                shark_img = shark_img_ul
+                shark_rect = shark_img_ul_rect
+
+            elif event.key == K_DOWN and keys[K_RIGHT] != False:
+                shark_img = shark_img_dr
+                shark_rect = shark_img_dr_rect
+
+            elif event.key == K_DOWN and keys[K_LEFT] != False:
+                shark_img = shark_img_dl
+                shark_rect = shark_img_dl_rect
+
+            elif event.key == K_RIGHT and keys[K_UP] != False:
+                shark_img = shark_img_ur
+                shark_rect = shark_img_ur_rect
+
+            elif event.key == K_LEFT and keys[K_UP] != False:
+                shark_img = shark_img_ul
+                shark_rect = shark_img_ul_rect
+
+            elif event.key == K_RIGHT and keys[K_DOWN] != False:
+                shark_img = shark_img_dr
+                shark_rect = shark_img_dr_rect
+
+            elif event.key == K_LEFT and keys[K_DOWN] != False:
+                shark_img = shark_img_dl
+                shark_rect = shark_img_dl_rect
+
+        if event.type == KEYUP:
+
+            if event.key == K_LEFT and keys[K_UP] != False:
+                shark_img = shark_img_u
+                shark_rect = shark_img_u_rect
+
+            elif event.key == K_UP and keys[K_LEFT] != False:
+                shark_img = shark_img_l
+                shark_rect = shark_img_l_rect
+
+            elif event.key == K_UP and keys[K_RIGHT] != False:
+                shark_img = shark_img_r
+                shark_rect = shark_img_r_rect
+
+            elif event.key == K_RIGHT and keys[K_UP] != False:
+                shark_img = shark_img_u
+                shark_rect = shark_img_u_rect
+
+            elif event.key == K_DOWN and keys[K_LEFT] != False:
+                shark_img = shark_img_l
+                shark_rect = shark_img_l_rect
+
+            elif event.key == K_LEFT and keys[K_DOWN] != False:
+                shark_img = shark_img_d
+                shark_rect = shark_img_d_rect
+
+            elif event.key == K_DOWN and keys[K_RIGHT] != False:
+                shark_img = shark_img_r
+                shark_rect = shark_img_r_rect
+
+            elif event.key == K_RIGHT and keys[K_DOWN] != False:
+                shark_img = shark_img_d
+                shark_rect = shark_img_d_rect
+            
 
     pos[0] += keys[K_RIGHT] - keys[K_LEFT]
     pos[1] += keys[K_DOWN] - keys[K_UP]
