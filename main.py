069bf690a0af410e186f4af78af0ac33623ac9a1
Diogo Vasconcelos <diogovasc99@gmail.com>
2022-03-17 16:10:28 +0000

    background transparente
    + other stuff

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ from pygame.locals import (
 # initialize game engine
 pygame.init()
 
-window_width=612
-window_height=437
+window_width=1009 #612
+window_height=720 #437
 
 step=10
 clock_tick_rate=120
@@ -35,10 +35,12 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Shark Bait")
 
 clock = pygame.time.Clock()
-background_image = pygame.image.load("ocean_background.png").convert()
+background_image_pre = pygame.image.load("ocean_background.png").convert()
+background_image = pygame.transform.rotozoom(background_image_pre, 0, 1.648)
+background_image.convert_alpha()
 
 # Load Shark Right
-shark_img_pre_r = pygame.image.load('shark_model_right.png')
+shark_img_pre_r = pygame.image.load('img_nobg/shark_model_right.png')
 shark_img_r = pygame.transform.rotozoom(shark_img_pre_r, 0, 0.1)
 shark_img_r.convert_alpha()
 shark_img_r_rect = shark_img_r.get_rect()
@@ -46,44 +48,44 @@ shark_x = window_width//2
 shark_y = window_height//2
 
 # Load Shark Left
-shark_img_pre_l = pygame.image.load('shark_model_left.png')
+shark_img_pre_l = pygame.image.load('img_nobg/shark_model_left.png')
 shark_img_l = pygame.transform.rotozoom(shark_img_pre_l, 0, 0.1)
 shark_img_l.convert_alpha()
 shark_img_l_rect = shark_img_l.get_rect()
 
 # Load Shark Up
-shark_img_pre_u = pygame.image.load('shark_model_up.png')
+shark_img_pre_u = pygame.image.load('img_nobg/shark_model_up.png')
 shark_img_u = pygame.transform.rotozoom(shark_img_pre_u, 0, 0.1)
 shark_img_u.convert_alpha()
 shark_img_u_rect = shark_img_u.get_rect()
 
 # Load Shark Down
-shark_img_pre_d = pygame.image.load('shark_model_down.png')
+shark_img_pre_d = pygame.image.load('img_nobg/shark_model_down.png')
 shark_img_d = pygame.transform.rotozoom(shark_img_pre_d, 0, 0.1)
 shark_img_d.convert_alpha()
 shark_img_d_rect = shark_img_d.get_rect()
 
 # Load Shark Up Right
-shark_img_pre_ur = pygame.image.load('shark_model_up_right.png')
-shark_img_ur = pygame.transform.rotozoom(shark_img_pre_ur, 0, 0.1)
+shark_img_pre_ur = pygame.image.load('img_nobg/shark_model_up_right.png')
+shark_img_ur = pygame.transform.rotozoom(shark_img_pre_ur, 0, 0.14)
 shark_img_ur.convert_alpha()
 shark_img_ur_rect = shark_img_ur.get_rect()
 
 # Load Shark Up Left
-shark_img_pre_ul = pygame.image.load('shark_model_up_left.png')
-shark_img_ul = pygame.transform.rotozoom(shark_img_pre_ul, 0, 0.1)
+shark_img_pre_ul = pygame.image.load('img_nobg/shark_model_up_left.png')
+shark_img_ul = pygame.transform.rotozoom(shark_img_pre_ul, 0, 0.14)
 shark_img_ul.convert_alpha()
 shark_img_ul_rect = shark_img_ul.get_rect()
 
 # Load Shark Down Right
-shark_img_pre_dr = pygame.image.load('shark_model_down_right.png')
-shark_img_dr = pygame.transform.rotozoom(shark_img_pre_dr, 0, 0.1)
+shark_img_pre_dr = pygame.image.load('img_nobg/shark_model_down_right.png')
+shark_img_dr = pygame.transform.rotozoom(shark_img_pre_dr, 0, 0.14)
 shark_img_dr.convert_alpha()
 shark_img_dr_rect = shark_img_dr.get_rect()
 
 # Load Shark Down Left
-shark_img_pre_dl = pygame.image.load('shark_model_down_left.png')
-shark_img_dl = pygame.transform.rotozoom(shark_img_pre_dl, 0, 0.1)
+shark_img_pre_dl = pygame.image.load('img_nobg/shark_model_down_left.png')
+shark_img_dl = pygame.transform.rotozoom(shark_img_pre_dl, 0, 0.14)
 shark_img_dl.convert_alpha()
 shark_img_dl_rect = shark_img_dl.get_rect()
 
@@ -94,11 +96,11 @@ fish_coords = {}
 fish_imgs = []
 fish_rects = []
 while i < fish_num:
-    fish_x = randint(0, 600)
-    fish_y = randint(0, 420)
+    fish_x = randint(0, window_width-15)
+    fish_y = randint(0, window_height-15)
     fish_coords[i] = [fish_x, fish_y]
-    fish_img_pre = pygame.image.load('fish_model.png')
-    fish_img = pygame.transform.rotozoom(fish_img_pre, 0, 0.02)
+    fish_img_pre = pygame.image.load('img_nobg/fish_model.png')
+    fish_img = pygame.transform.rotozoom(fish_img_pre, 0, 0.06)
     fish_img.convert_alpha()
     fish_imgs.append(fish_img)
     fish_rects.append(fish_img.get_rect())
@@ -203,8 +205,12 @@ while dead != True:
                 shark_rect = shark_img_d_rect
             
 
-    pos[0] += keys[K_RIGHT] - keys[K_LEFT]
-    pos[1] += keys[K_DOWN] - keys[K_UP]
+    
+    if (pos[0] + keys[K_RIGHT] - keys[K_LEFT] > 0 and pos[0] + keys[K_RIGHT] - keys[K_LEFT] < window_width-shark_img.get_width()):
+        pos[0] += keys[K_RIGHT] - keys[K_LEFT]
+    if (pos[1] + keys[K_DOWN] - keys[K_UP] > 0 and pos[1] + keys[K_DOWN] - keys[K_UP] < window_height-shark_img.get_height()):
+        pos[1] += keys[K_DOWN] - keys[K_UP]
+    
     screen.blit(background_image, [0, 0])
     screen.blit(shark_img, pos)
     i = 0
