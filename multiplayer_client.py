ebfc2c2abc23c186f9290b41e28de4f25434dfaa
asergiogomes <ae3654@alunos.uminho.pt>
2022-04-06 17:47:38 +0100

    multiplayer_client para orientado a objectos

--- multiplayer_client.py
+++ multiplayer_client.py
@@ -1,33 +1,51 @@
 # multicast receiver in client side
 import socket
 import struct
-import time
 import json
 
-while True:
-    # Initialise socket for IPv6 datagrams
-    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+class Client():
 
-    # Allows address to be reused
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    receive_buffer = {}
+    send_buffer = []
 
-    # Binds to all interfaces on the given port
-    sock.bind(('', 8080))
+    def __init__(self, dict, list):
+        self.receive_buffer = dict
+        self.send_buffer = list
 
-    # Allow messages from this socket to loop back for development
-    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
+    def load_receive_buffer(self, dict):
+        self.buffer = dict
 
-    # Construct message for joining multicast group
-    mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, "ff02::abcd:1"), (chr(0) * 16).encode('utf-8'))
-    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+    def load_send_buffer(self, list):
+        self.send_buffer = list
 
-    data, addr = sock.recvfrom(2048)
+    def receive(self):
+        # Initialise socket for IPv6 datagrams
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 
-    utf8_string = data.decode('utf-8')
+        # Allows address to be reused
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    print(type(utf8_string))
+        # Binds to all interfaces on the given port
+        sock.bind(('', 8080))
 
-    JSON_string = json.loads(utf8_string)
+        # Allow messages from this socket to loop back for development
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)
 
-    print(JSON_string)
+        # Construct message for joining multicast group
+        mreq = struct.pack("16s15s".encode('utf-8'), socket.inet_pton(socket.AF_INET6, "ff02::abcd:1"), (chr(0) * 16).encode('utf-8'))
+        sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_JOIN_GROUP, mreq)
+
+        data, addr = sock.recvfrom(2048)
+
+        utf8_string = data.decode('utf-8')
+
+        print(type(utf8_string))
+
+        JSON_string = json.loads(utf8_string)
+
+        self.load_buffer()
+
+    #parte que envia ao servidor as coordenadas e orientação
+    def send(self):
+        return
     
